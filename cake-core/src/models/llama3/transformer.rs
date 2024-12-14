ac0aea605e36272651041eb15528dfd87bad0772
parthdev99 <105319463+parthdev99@users.noreply.github.com>
2024-12-14 16:45:44 +0530

    Update transformer.rs

--- cake-core/src/models/llama3/transformer.rs
+++ cake-core/src/models/llama3/transformer.rs
@@ -33,6 +33,9 @@ impl Forwarder for Transformer {
             .pp(&name);
         let cfg = ctx.config.as_ref().expect("No config specified");
 
+        log::info!("Loading Model Layer: {}", String);
+        log::info!("Loading self attention with path: {}", vb.pp("self_attn"));
+
         let attn = super::CausalSelfAttention::load(vb.pp("self_attn"), cfg)?;
         let mlp = super::MLP::load(vb.pp("mlp"), cfg)?;
         let rms_1 =
